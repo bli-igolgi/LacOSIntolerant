9145a2c21ba587ef7c9d59fe32d6ab88f79d4aef
Guldner <cguldne2@illinois.edu>
2017-03-10 13:53:15 -0600

    Add fix script for tmpmp3 error

---
+++ student-distrib/tmpmp3_fix.sh
@@ -0,0 +1,6 @@
+# If there is an error that says /mnt/tmpmp3 already exists
+# run this script, and in git bash run 'git checkout student-distrib/mp3.img'
+sudo rm -rf /mnt/tmpmp3
+rm bootimg
+rm mp3.img
+git checkout mp3.img

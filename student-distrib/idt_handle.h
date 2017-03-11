015e5390d84a96c07e93a91273e1c0a86f61e5fd
Guldner <cguldne2@illinois.edu>
2017-03-11 13:40:11 -0600

    Change to space indent

--- student-distrib/idt_handle.h
+++ student-distrib/idt_handle.h
@@ -6,19 +6,20 @@
 #include "x86_desc.h"
 #include "lib.h"
 
-//	designate idt_desc_t struct as trap gate with default values
-#define SET_TRAP_GATE(str)		do {\
-	str.seg_selector = KERNEL_CS;	\
-	str.reserved3 = 1;	\
-    str.reserved2 = 1;	\
-    str.reserved1 = 1;	\
-    str.size = 1;	\
-	str.reserved0 = 0;	\
-    str.dpl = 0;	\
-    str.present = 1;	\
+//  designate idt_desc_t struct as trap gate with default values
+#define SET_TRAP_GATE(str)
+do { \
+    str.seg_selector = KERNEL_CS;   \
+    str.reserved3 = 1;  \
+    str.reserved2 = 1;  \
+    str.reserved1 = 1;  \
+    str.size = 1;       \
+    str.reserved0 = 0;  \
+    str.dpl = 0;        \
+    str.present = 1;    \
 } while(0)
 
-//	initializes interrupt descriptor table
+//  initializes interrupt descriptor table
 extern void idt_init(void);
 
 extern void div_zero_fault(void);

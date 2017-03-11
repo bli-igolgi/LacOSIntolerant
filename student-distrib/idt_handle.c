85ccfc63864e81a9d0bb7f9d5a8ff4962f8c4447
Guldner <cguldne2@illinois.edu>
2017-03-11 13:40:22 -0600

    Change to space indent

--- student-distrib/idt_handle.c
+++ student-distrib/idt_handle.c
@@ -1,21 +1,21 @@
 #include "idt_set.h"
 
 /*
-	function header here 
+    function header here 
 */
 void idt_init(){
-	idt_desc_t idt_entry;
-	
-	SET_TRAP_GATE(idt_entry);
-	
-	SET_IDT_ENTRY(idt_entry, _divide_by_zero);
+    idt_desc_t idt_entry;
+    
+    SET_TRAP_GATE(idt_entry);
+    
+    SET_IDT_ENTRY(idt_entry, _divide_by_zero);
 
     idt[0] = idt_entry;
     return;
 }
 
 /*
-	function header here 
+    function header here 
 */
 void div_zero_fault() {
     printf("Divide-by-zero fault occurred. Do you even math?");
@@ -24,7 +24,7 @@ void div_zero_fault() {
 }
 
 /*
-	function header here 
+    function header here 
 */
 void debug_trap() {
     printf("Debug exception thrown. Waiting indefinitely...");

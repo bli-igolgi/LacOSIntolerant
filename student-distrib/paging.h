4990aa235832a6cea4c79d4c000ac0f6fdf79e0d
Mahir Morshed <mmorshe2@illinois.edu>
2017-03-11 16:15:49 -0600

    improved map_page ever so slightly

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -3,22 +3,19 @@
 
 #include "types.h"
 
-
+#define SIZEOFDIR 4096       // size of small page, page directory, page table.... (4kB)
 #define NUM_ENTRIES 1024     // the number of entries in a page table or page directory
 #define KERNAL_ADDR 0x8000   // address of the kernal in physical memory
 #define VIDEO_ADDR 0         // address of the video memory in physical memory
 
 // global variable for the page directory
-int32_t page_directory[NUM_ENTRIES] __attribute__((aligned (4096)));
+uint32_t page_directory[NUM_ENTRIES] __attribute__((aligned (SIZEOFDIR)));
 
 
 // functions related to paging
 extern void * resolve_virt_addr(void * virt_addr);
 extern void paging_init();
 extern int map_page(void * phys_addr, void * virtual_addr, int page_size, int privileges, int write);
-
-
-#include "paging.c"
 
 #endif
 

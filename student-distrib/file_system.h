b40f065b2c09fd94624dc00b416583888dd82ca9
Guldner <cguldne2@illinois.edu>
2017-03-15 11:46:48 -0500

    Add implementation comments

--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -12,5 +12,6 @@ uint32_t file_write();
 
 // Reading from directory should read all file names and add to buffer, including '.'
 // Reading from file should read all of the bytes and add to buffer, even non-printable characters
+// inode0 is NULL
 
 #endif

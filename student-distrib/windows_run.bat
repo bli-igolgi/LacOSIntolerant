eadf978986270a3a19a99d071168e3f849607b7a
cguldne2 <cguldne2@illinois.edu>
2017-03-20 15:05:48 -0500

    Revert windows_run.bat to regular version

--- student-distrib/windows_run.bat
+++ student-distrib/windows_run.bat
@@ -1,4 +1,4 @@
 :: Run this on windows to open the test_debug
 c:
-cd "C:\Users\Cody\Sync\My Documents\School\UIUC\ece391\qemu_win"
-qemu-system-i386w.exe -hda "C:\Users\Cody\Sync\My Documents\School\UIUC\ece391\ece391_share\work\mp3\student-distrib\mp3.img" -m 256 -gdb tcp:127.0.0.1:1234 -S -name mp3
+cd "c:\qemu-1.5.0-win32-sdl\"
+qemu-system-i386w.exe -hda Z:\mp3\student-distrib\mp3.img -m 256 -gdb tcp:127.0.0.1:1234 -S -name mp3

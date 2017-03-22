c7e817620179ec2ea8aa52a7af6e31380a16d45f
Cody Guldner <burningwolf96@gmail.com>
2017-03-21 22:14:09 -0500

    Start filesystem

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -44,3 +44,33 @@ int32_t file_write(int32_t fd, const void *buf, int32_t nbytes) {
     return FAILURE;
 }
 
+/*
+ * int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+ *   Inputs: 
+ *   Return Value: 
+ *   Function: 
+ */
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry) {
+    return FAILURE;
+}
+
+/*
+ * int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+ *   Inputs: 
+ *   Return Value: 
+ *   Function: 
+ */
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry) {
+    return FAILURE;
+}
+
+/*
+ * int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+ *   Inputs: 
+ *   Return Value: 
+ *   Function: 
+ */
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length) {
+    return FAILURE;
+}
+
--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -3,17 +3,21 @@
 
 #include "driver.h"
 
-struct file_desc {
+typedef struct dentry_t {
 	uint32_t file_name[8];     // File name is 32 bytes
 	uint32_t file_type;
 	uint32_t inode_num;
 	uint32_t reserved[6];      // Reserved section is 24 bytes
-};
+} dentry_t;
 
 extern int32_t file_open(const uint8_t* filename);
 extern int32_t file_close(int32_t fd);
 extern int32_t file_read(int32_t fd, void *buf, int32_t nbytes);
 extern int32_t file_write(int32_t fd, const void *buf, int32_t nbytes);
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 
 // Reading from directory should read all file names and add to buffer, including '.'
 // Reading from file should read all of the bytes and add to buffer, even non-printable characters

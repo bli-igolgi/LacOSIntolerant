c075debd49aa1002eee52bf053c6b4ea0e64bccc
Cody <burningwolf96@gmail.com>
2017-04-07 17:54:07 -0500

    No idea

--- student-distrib/syscalls.h
+++ student-distrib/syscalls.h
@@ -1,6 +1,18 @@
 #ifndef SYS_CALLS_H
 #define SYS_CALLS_H
 
+/* The vector IDT numbers for the system calls */
+#define SYS_HALT    1
+#define SYS_EXECUTE 2
+#define SYS_READ    3
+#define SYS_WRITE   4
+#define SYS_OPEN    5
+#define SYS_CLOSE   6
+#define SYS_GETARGS 7
+#define SYS_VIDMAP  8
+#define SYS_SET_HANDLER  9
+#define SYS_SIGRETURN  10
+
 #ifndef ASM
 
 /* The C wrapper functions for the system calls */

9b56c0fa0f6fcc0ec1b2331c77aeec0ea73fe52c
Cody <burningwolf96@gmail.com>
2017-04-07 16:59:50 -0500

    SOme fixes, not sure what to do

--- student-distrib/syscalls.h
+++ student-distrib/syscalls.h
@@ -3,6 +3,7 @@
 
 #ifndef ASM
 
+/* The C wrapper functions for the system calls */
 int32_t halt(uint8_t status);
 int32_t execute(const uint8_t *command);
 int32_t read(int32_t fd, void *buf, int32_t nbytes);
@@ -14,15 +15,16 @@ int32_t vidmap(uint8_t **screen_start);
 int32_t set_handler(int32_t signum, void *handler_address);
 int32_t sigreturn(void);
 
+/* The actual system calls */
 asmlinkage int32_t sys_halt(uint8_t status);
 asmlinkage int32_t sys_execute(const uint8_t *command);
-asmlinkage int32_t sys_read(asmlinkage int32_t fd, void *buf, asmlinkage int32_t nbytes);
-asmlinkage int32_t sys_write(asmlinkage int32_t fd, const void *buf, asmlinkage int32_t nbytes);
+asmlinkage int32_t sys_read(int32_t fd, void *buf, int32_t nbytes);
+asmlinkage int32_t sys_write(int32_t fd, const void *buf, int32_t nbytes);
 asmlinkage int32_t sys_open(const uint8_t *filename);
-asmlinkage int32_t sys_lose(asmlinkage int32_t fd);
-asmlinkage int32_t sys_getargs(uint8_t *buf, asmlinkage int32_t nbytes);
+asmlinkage int32_t sys_lose(int32_t fd);
+asmlinkage int32_t sys_getargs(uint8_t *buf, int32_t nbytes);
 asmlinkage int32_t sys_vidmap(uint8_t **screen_start);
-asmlinkage int32_t sys_set_handler(asmlinkage int32_t signum, void *handler_address);
+asmlinkage int32_t sys_set_handler(int32_t signum, void *handler_address);
 asmlinkage int32_t sys_sigreturn(void);
 
 

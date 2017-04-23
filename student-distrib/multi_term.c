b045b3168d5138d37582a56ac3c2ebcd63222db2
Cody <burningwolf96@gmail.com>
2017-04-23 11:48:40 -0500

    Add function interfaces

--- student-distrib/multi_term.c
+++ student-distrib/multi_term.c
@@ -10,6 +10,12 @@ static char* video_mem = (char *)VIDEO_ADDR;
 term_t terminals[MAX_TERM_NUM];
 int cur_term_id = 0;
 
+/*
+ * void multi_term_init();
+ *   Inputs: none
+ *   Return Value: none
+ *   Function: Initializes the array of terminals
+ */
 void multi_term_init() {
     uint32_t i;
     for(i = 0; i < MAX_TERM_NUM; i++){
@@ -22,6 +28,13 @@ void multi_term_init() {
     }
 }
 
+/*
+ * void switch_terminal(int new_term_id);
+ *   Inputs: new_term_id - the ID of the terminal to switch to
+ *   Return Value: none
+ *   Function: Switches from the current terminal to the new terminal,
+ *               updating video memory and switching to the other process
+ */
 void switch_terminal(int new_term_id) {
     terminals[cur_term_id].key_x = screen_x;
     terminals[cur_term_id].key_y = screen_y;
@@ -35,6 +48,13 @@ void switch_terminal(int new_term_id) {
     switch_stackframe(new_term_id);
 }
 
+/*
+ * void switch_screen(int new_term_id);
+ *   Inputs: new_term_id - the ID of the terminal to switch to
+ *   Return Value: none
+ *   Function: Copies video memory into the current terminal,
+ *              and copies the new terminal's old screen to video memory 
+ */
 void switch_screen(int new_term_id) {
     int32_t i;
     uint8_t *cur_vid = terminals[cur_term_id].vid_mem,
@@ -45,7 +65,14 @@ void switch_screen(int new_term_id) {
     }
 }
 
+/*
+ * void switch_stackframe(int new_term_id);
+ *   Inputs: new_term_id - the ID of the terminal to switch to
+ *   Return Value: none
+ *   Function: Switches the stack frame to the new terminal
+ */
 void switch_stackframe(int new_term_id) {
+    // Push registers and save the esp and ebp
     asm volatile(
         "pushal;"
         "movl %%esp, %0;"
@@ -53,12 +80,14 @@ void switch_stackframe(int new_term_id) {
         :"=m"(terminals[cur_term_id].esp), "=m"(terminals[cur_term_id].ebp)
     );
     cur_term_id = new_term_id;
+    // If this terminal hasn't been run yet, start shell on it
     if(!terminals[new_term_id].cur_task) {
         // Send end of interrupt for the keyboard
         send_eoi(1);
         sys_execute((uint8_t *)"shell");
     }
     else {
+        // Move the old stack frame back and pop registers to return to old task
         asm volatile(
             "movl %0, %%esp;"
             "movl %1, %%ebp;"
--- student-distrib/multi_term.h
+++ student-distrib/multi_term.h
@@ -18,13 +18,13 @@ typedef struct term_t {
     uint8_t key_buf[BUF_SIZE+1];            // Buffer for the input data from the keyboard
     uint32_t key_buf_index;
     uint32_t esp, ebp;                      // The current terminals esp and ebp
-    struct pcb_t *cur_task;
+    struct pcb_t *cur_task;                 // The task that is currently executing on the terminal
 } term_t;
 
 extern term_t terminals[3];
 extern int cur_term_id;
 
-void multi_term_init();
+void multi_term_init(void);
 void switch_terminal(int new_term_id);
 void switch_screen(int new_term_id);
 void switch_stackframe(int new_term_id);

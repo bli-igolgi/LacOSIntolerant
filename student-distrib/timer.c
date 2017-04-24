00c2ccfe0845361225b8439f9d5b6494cee480ee
Cody <burningwolf96@gmail.com>
2017-04-24 16:42:25 -0500

    Work on trying to multitask

--- student-distrib/timer.c
+++ student-distrib/timer.c
@@ -27,7 +27,7 @@ void pit_init() {
  *   Function: Sets the PIT to receive interrupts hz times every second
  */
 void set_pit_freq(uint32_t hz) {
-    int divisor = 1193180 / hz;            /* Calculate our divisor */
+    int divisor = BASE_FREQ / hz;          /* Calculate our divisor */
     outb(0x36, COMMAND_PORT);              /* Set our command byte 0x36 */
     outb(divisor & 0xFF, CHANNEL1_PORT);   /* Set low byte of divisor */
     outb(divisor >> 8, CHANNEL1_PORT);     /* Set high byte of divisor */
@@ -44,10 +44,46 @@ void pit_interrupt() {
 
     // Perform the task switch
     if(++timer_ticks >= TIME_QUANTUM) {
-        timer_ticks = 0;
         // printf("%d milliseconds has passed\n", TIME_QUANTUM);
+        timer_ticks = 0;
+        // uint8_t next_tid = (cur_term_id + 1) % MAX_TERM_NUM;
+
+        // if(terminals[next_tid].cur_task) switch_tasks(next_tid);
+        // else if(terminals[(next_tid = ((next_tid + 1) % MAX_TERM_NUM))].cur_task)
+        //     switch_tasks(next_tid);
     }
 
-    sti();
     send_eoi(PIT_IRQ);
+    sti();
+}
+
+void switch_tasks(int new_term_id) {
+    if(terminals[new_term_id].cur_task && new_term_id != cur_term_id) {
+        // Push registers and save the esp and ebp
+        asm volatile(
+            //"pushal;"
+            "movl %%esp, %0;"
+            "movl %%ebp, %1;"
+            :"=r"(terminals[cur_term_id].esp), "=r"(terminals[cur_term_id].ebp)
+        );
+        cur_term_id = new_term_id;
+        // If this terminal hasn't been run yet, start shell on it
+        cur_pcb = terminals[new_term_id].cur_task;
+
+        map_page((void *)(cur_pcb->page_addr), (void *)PROGRAM_VIRT, true, true, true, true);
+        flush_tlb();
+        tss.esp0 = cur_pcb->esp0;
+        tss.ss0 = cur_pcb->ss0;
+        sti();
+        // Move the old stack frame back and pop registers to return to old task
+        asm volatile(
+            "movl %0, %%esp;"
+            "movl %1, %%ebp;"
+            //"popal"
+            ""
+            : 
+            : "r"(terminals[new_term_id].esp), "r"(terminals[new_term_id].ebp)
+        );
+    }
+    return;
 }
